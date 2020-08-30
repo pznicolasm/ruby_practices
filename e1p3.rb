866aac62b6228249e1d8432f4d3b76d98b61ceb4
Nicolas Perez <52212129+pznicolasm@users.noreply.github.com>
2020-08-30 16:53:07 -0300

    Delete e1p3.rb

--- e1p3.rb
+++
@@ -1,8 +0,0 @@
-#Si listamos todos los números naturales menores que 10 que son múltiplos de 3 o 5
-#obtenemos 3, 5, 6 y 9. La suma de todos estos números es 23. Encontrá la suma de todos
-#los múltiplos de 3 o 5 menores que 1000.
-
-
-suma = ((0..1000).select { |n| n%3 == 0 || n%5 == 0}).inject(0,:+)
-puts "la suma de los multiplos de 3 y 5 da: ", suma
-
